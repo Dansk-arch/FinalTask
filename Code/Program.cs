93abe7c61e6a356e03d9a6b3fcda2b3322e7a3b4
Daniil_Korolev <dan.s.korolev@gmail.com>
2023-08-05 20:24:38 +0300

    Initial commit and add code

---
+++ Code/Program.cs
@@ -0,0 +1,57 @@
+﻿/*Написать программу, которая 
+из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+string[] GetArrayWithFilterLength(string[] Array, int LengthValue)
+{
+    int size = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= LengthValue)
+        {
+            size++;
+        }
+    }
+    //Console.WriteLine(size);
+    string[] ArrayForSelect = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        ArrayForSelect[i] = Array[i];
+    }
+    return ArrayForSelect;
+}
+
+string[] SortedArrayWithStrings(string[] Array)
+{
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        string temp = "";
+        for (int k = i + 1; k < Array.Length; k++)
+        {
+            if (Array[i].Length >= Array[k].Length)
+            {
+                temp = Array[i];
+                Array[i] = Array[k];
+                Array[k] = temp;
+            }
+        }
+
+    }
+    return Array;
+}
+
+string PrintArray(string[] array)
+{
+    return string.Join(", ", array);
+}
+
+
+string[] Array = { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russuia", "Kazan"};
+int LengthValueForSelect = 3; //условие задачи
+string[] SortedArray = SortedArrayWithStrings(Array);
+//Console.WriteLine(PrintArray(SortedArray));
+string[] FiltredArray = GetArrayWithFilterLength(SortedArray, LengthValueForSelect);
+Console.WriteLine(PrintArray(FiltredArray));
